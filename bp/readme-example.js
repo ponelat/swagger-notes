1a04124a62f5e520ad0eb6bf0f9817877146d8df
Josh Ponelat <josh@ponelat.com>
2015-06-01 09:35:19 +0200

    update bp code

---
+++ bp/readme-example.js
@@ -0,0 +1,10 @@
+var client = require('swagger-client');
+
+var swagger = new client({
+  url: 'http://petstore.swagger.io/v2/swagger.json',
+  success: function() {
+    swagger.pet.getPetById({petId:7},{responseContentType: 'application/xml'},function(pet){
+      console.log('pet', pet);
+    });
+  }
+})
