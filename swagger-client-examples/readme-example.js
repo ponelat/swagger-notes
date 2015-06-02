b8b0c9c41f785d81ae50f1494c34ff25efb24f40
Josh Ponelat <josh@ponelat.com>
2015-06-02 13:09:06 +0200

    update readme example

--- swagger-client-examples/readme-example.js
+++ swagger-client-examples/readme-example.js
@@ -3,8 +3,8 @@ var client = require('swagger-client');
 var swagger = new client({
   url: 'http://petstore.swagger.io/v2/swagger.json',
   success: function() {
-    swagger.pet.getPetById({petId:7},{responseContentType: 'application/xml'},function(pet){
+    swagger.pet.getPetById({petId:7},{responseContentType: 'application/json'},function(pet){
       console.log('pet', pet);
     });
   }
-})
+});
